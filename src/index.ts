9706a1a2eabcde9250273ba3deb793ea3a2458a2
chingwu99 <henry88028802@hotmail.com>
2024-02-17 17:58:11 +0800

    fix: update domain

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-  console.log("server running on http://localhost:8080/");
+server.listen(`https://wild-pink-giraffe-shoe.cyclic.app`, () => {
+  console.log("server running on https://wild-pink-giraffe-shoe.cyclic.app/");
   //   console.log(process.env.DATABASE_URL);
 });
 
